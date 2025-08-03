8fe0923fdc8c2e21244c2feae9c9ce83986702b7
Joshua Freudenhammer <jfreuden@alumni.cmu.edu>
2025-08-03 15:19:00 +0200

    It doesn't work and the brute force doesn't work either

--- progress/bowlstack/src/main.rs
+++ progress/bowlstack/src/main.rs
@@ -36,7 +36,7 @@ where
 }
 
 type BowlInt = u32;
-type BowlFloat = f32;
+type BowlFloat = f64;
 
 #[derive(Debug, Copy, Clone)]
 struct Bowl {
@@ -96,105 +96,191 @@ const CMP_STEEPER: fn(&Bowl, &Bowl) -> core::cmp::Ordering = |a, b| {
 #[allow(dead_code)]
 const CMP_SHALLOWER: fn(&Bowl, &Bowl) -> core::cmp::Ordering = |a, b| CMP_STEEPER(b, a);
 
-impl Solvable<&mut Vec<Bowl>> for &mut Vec<Bowl> {
-    fn solve(bowls: &mut Vec<Bowl>) -> BowlFloat {
-        #[inline]
-        fn y(m: BowlFloat, x: BowlFloat, b: BowlFloat) -> BowlFloat {
-            m * x + b
+#[inline]
+fn y(m: BowlFloat, x: BowlFloat, b: BowlFloat) -> BowlFloat {
+    m * x + b
+}
+
+#[inline]
+fn b(m1: BowlFloat, m2: BowlFloat, x: BowlFloat) -> BowlFloat {
+    (m1 - m2) * x
+}
+
+fn find_gap(bottom: &Bowl, top: &Bowl) -> BowlFloat {
+    let h1 = bottom.height as BowlFloat;
+    let h2 = top.height as BowlFloat;
+    let r1 = bottom.bottom_radius as BowlFloat;
+    let r2 = top.bottom_radius as BowlFloat;
+    let R1 = bottom.top_radius as BowlFloat;
+    let R2 = top.top_radius as BowlFloat;
+    let m1 = bottom.slope();
+    let m2 = top.slope();
+
+    let p11x = BowlFloat::default();
+    let p12x = R1 - r1;
+    let p21x = r2 - r1;
+    let p22x = R2 - r1;
+
+    let p11y = BowlFloat::default();
+    let p12y = y(m1, p12x, BowlFloat::default());
+    let b_floor = -y(m2, p21x, BowlFloat::default());
+
+    let b2 = if r2 <= r1 && y(m2, p22x, b_floor) >= y(m1, p22x, BowlFloat::default()) {
+        b_floor // Floor case -------- had let p21y = BowlFloat::default(); here, unsure if needed
+    } else if r2 >= R1 {
+        b_floor + h2 // Ceiling case (or rim case, if you prefer)
+    } else if m2 >= m1 {
+        if m2 == m1 {
+            BowlFloat::default() // Special case: for identical slopes, b_2 is zero
+        } else {
+            b(m1, m2, p21x) // Steeper only allows P_21 case
+        }
+    } else if m2 < m1 {
+        // Shallow could be P_12 case or P_22 case
+        if R2 > R1 {
+            b(m1, m2, p12x) // P_12 case
+        } else {
+            b(m1, m2, p22x) // P_22 case
+        }
+    } else {
+        panic!(
+            "Holy shit! We are all gonna die, this case shouldn't happen, holy fucking hell, someone take a look at these demon bowls and tell me what's wrong! {:?}, {:?}",
+            bottom, top
+        )
+    };
+
+    let p21y = y(m2, p21x, b2);
+    let p22y = y(m2, p22x, b2);
+
+    // Do I even need to check that P22 is higher than P12?
+    // In theory, the gap is simply p21y, and the contact points were preselected.
+    p21y
+}
+
+fn solve_stack_for_sort(
+    bowls: &mut Vec<Bowl>,
+    sort: fn(&Bowl, &Bowl) -> core::cmp::Ordering,
+) -> BowlFloat {
+    bowls.sort_by(sort);
+    
+    // TODO: if a bowl is so tiny that it could sit inside the final bowl, it doesn't matter what it's slope is
+    
+    let mut stackheight = bowls.first().unwrap().height as BowlFloat;
+    let mut highestbottom = BowlFloat::default();
+    let mut highestbowl = bowls.first().unwrap().clone();
+    for window in bowls.windows(2) {
+        let [bottom, top]: [Bowl; 2] = window.try_into().unwrap();
+        let gap_bottom = find_gap(&bottom, &top);
+
+        let synth_bowl = Bowl::from([
+            stackheight - highestbottom,
+            bottom.bottom_radius,
+            highestbowl.top_radius,
+        ]);
+
+        let gap_highest = find_gap(&synth_bowl, &top);
+        highestbottom += BowlFloat::max(gap_bottom, gap_highest);
+
+        let candidate = highestbottom + top.height as BowlFloat;
+        if candidate > stackheight {
+            stackheight = candidate;
+            highestbowl = top.clone();
         }
 
-        #[inline]
-        fn b(m1: BowlFloat, m2: BowlFloat, x: BowlFloat) -> BowlFloat {
-            (m1 - m2) * x
-        }
+        stackheight = BowlFloat::max(candidate, stackheight);
+    }
+    stackheight
+}
 
-        fn find_gap(bottom: &Bowl, top: &Bowl) -> BowlFloat {
-            let h1 = bottom.height as BowlFloat;
-            let h2 = top.height as BowlFloat;
-            let r1 = bottom.bottom_radius as BowlFloat;
-            let r2 = top.bottom_radius as BowlFloat;
-            let R1 = bottom.top_radius as BowlFloat;
-            let R2 = top.top_radius as BowlFloat;
-            let m1 = bottom.slope();
-            let m2 = top.slope();
+trait Permutations<T> {
+    fn map_permutations<F, R>(&self, f: F) -> Vec<R>
+    where
+        F: Fn(&[T]) -> R,
+        T: Clone;
+}
 
-            let p11x = BowlFloat::default();
-            let p12x = R1 - r1;
-            let p21x = r2 - r1;
-            let p22x = R2 - r1;
-
-            let p11y = BowlFloat::default();
-            let p12y = y(m1, p12x, BowlFloat::default());
-            let b_floor = -y(m2, p21x, BowlFloat::default());
-
-            let b2 = if r2 <= r1 && y(m2, p22x, b_floor) >= y(m1, p22x, BowlFloat::default()) {
-                b_floor // Floor case -------- had let p21y = BowlFloat::default(); here, unsure if needed
-            } else if r2 >= R1 {
-                b_floor + h2 // Ceiling case (or rim case, if you prefer)
-            } else if m2 >= m1 {
-                if m2 == m1 {
-                    BowlFloat::default() // Special case: for identical slopes, b_2 is zero
-                } else {
-                    b(m1, m2, p21x) // Steeper only allows P_21 case
-                }
-            } else if m2 < m1 {
-                // Shallow could be P_12 case or P_22 case
-                if R2 > R1 {
-                    b(m1, m2, p12x) // P_12 case
-                } else {
-                    b(m1, m2, p22x) // P_22 case
-                }
-            } else {
-                panic!(
-                    "Holy shit! We are all gonna die, this case shouldn't happen, holy fucking hell, someone take a look at these demon bowls and tell me what's wrong! {:?}, {:?}",
-                    bottom, top
-                )
-            };
-
-            let p21y = y(m2, p21x, b2);
-            let p22y = y(m2, p22x, b2);
-
-            // Do I even need to check that P22 is higher than P12?
-            // In theory, the gap is simply p21y, and the contact points were preselected.
-            p21y
-        }
-
-        fn solve_stack_for_sort(
-            bowls: &mut Vec<Bowl>,
-            sort: fn(&Bowl, &Bowl) -> core::cmp::Ordering,
-        ) -> f32 {
-            bowls.sort_by(sort);
-            let mut stackheight = bowls.first().unwrap().height as BowlFloat;
-            let mut highestbottom = BowlFloat::default();
-            let mut highestbowl = bowls.first().unwrap().clone();
-            for window in bowls.windows(2) {
-                let [bottom, top]: [Bowl; 2] = window.try_into().unwrap();
-                let gap_bottom = find_gap(&bottom, &top);
-
-                let synth_bowl = Bowl::from([
-                    stackheight - highestbottom,
-                    bottom.bottom_radius,
-                    highestbowl.top_radius,
-                ]);
-
-                let gap_highest = find_gap(&synth_bowl, &top);
-                highestbottom += BowlFloat::max(gap_bottom, gap_highest);
-
-                let candidate = highestbottom + top.height as BowlFloat;
-                if candidate > stackheight {
-                    stackheight = candidate;
-                    highestbowl = top.clone();
-                }
-
-                stackheight = BowlFloat::max(candidate, stackheight);
+impl<T> Permutations<T> for Vec<T> {
+    fn map_permutations<F, R>(&self, f: F) -> Vec<R>
+    where
+        F: Fn(&[T]) -> R,
+        T: Clone,
+    {
+        fn permute<T, F, R>(items: &[T], prefix: &mut Vec<T>, results: &mut Vec<R>, f: &F)
+        where
+            F: Fn(&[T]) -> R,
+            T: Clone,
+        {
+            if items.is_empty() {
+                results.push(f(prefix));
             }
-            stackheight
+            for i in 0..items.len() {
+                let mut remaining = items.to_vec();
+                let item = remaining.remove(i);
+                prefix.push(item);
+                permute(&remaining, prefix, results, f);
+                prefix.pop();
+            }
         }
 
+        let mut results = Vec::new();
+        let mut prefix = Vec::new();
+        permute(self, &mut prefix, &mut results, &f);
+        results
+    }
+}
+
+impl Solvable<&mut Vec<Bowl>> for &mut Vec<Bowl> {
+
+    fn solve(bowls: &mut Vec<Bowl>) -> BowlFloat {
         let shallower = solve_stack_for_sort(bowls, CMP_SHALLOWER);
         let steeper = solve_stack_for_sort(bowls, CMP_STEEPER);
         BowlFloat::min(shallower, steeper)
     }
+}
+
+
+
+/// A reference implementation solver for generating test cases.
+#[allow(unused_variables, dead_code)]
+fn brute_solve(bowls: &mut Vec<Bowl>) -> BowlFloat {
+    let all_stacksizes: Vec<BowlFloat> = bowls.map_permutations(|bowlstack| {
+        let mut stackheight = bowlstack.first().unwrap().height as BowlFloat;
+        let mut highestbottom = BowlFloat::default();
+        let mut highestbowl = bowlstack.first().unwrap().clone();
+        
+        
+        for (i, window) in bowlstack.windows(2).enumerate() {
+            let [bottom, top]: [Bowl; 2] = window.try_into().unwrap();
+            let gap_bottom = find_gap(&bottom, &top);
+
+
+            let previous_bowls = bowlstack.iter().take(i);
+            
+            let something = previous_bowls.map(|x| {
+                // This won't work. it's because the bowls aren't storing how high up they are like the highest bowls
+                
+                BowlFloat::default()
+            }).into_iter().reduce(BowlFloat::min).unwrap();
+            let synth_bowl = Bowl::from([
+                stackheight - highestbottom,
+                bottom.bottom_radius,
+                highestbowl.top_radius,
+            ]);
+
+            let gap_highest = find_gap(&synth_bowl, &top);
+            highestbottom += BowlFloat::max(gap_bottom, gap_highest);
+
+            let candidate = highestbottom + top.height as BowlFloat;
+            if candidate > stackheight {
+                stackheight = candidate;
+                highestbowl = top.clone();
+            }
+
+            stackheight = BowlFloat::max(candidate, stackheight);
+        }
+        stackheight
+    });
+    all_stacksizes.into_iter().reduce(BowlFloat::min).unwrap()
 }
 
 fn main() {
@@ -211,13 +297,13 @@ fn main() {
     }
 
     for mut bowls in case_bowls {
-        let stackheight = solve(&mut bowls).trunc() as u32;
+        let stackheight = brute_solve(&mut bowls).trunc() as u32;
         println!("{:?}", stackheight);
     }
 }
 
 #[cfg(test)]
-mod tests {
+mod bowlstack_tests {
     use super::*;
 
     #[test]
@@ -231,13 +317,13 @@ mod tests {
             ],
         ];
 
-        let answers = case_bowls.iter_mut().map(solve).collect::<Vec<f32>>();
+        let answers = case_bowls.iter_mut().map(brute_solve).collect::<Vec<BowlFloat>>();
         assert_eq!(answers, vec![70., 55.])
     }
 
     #[test]
     fn test_solve_nested_sameslope() {
-        let bowls = vec![
+        let mut bowls = vec![
             Bowl::from([30, 10, 40]),
             Bowl::from([10, 12, 22]),
             Bowl::from([10, 14, 24]),
@@ -247,37 +333,113 @@ mod tests {
             println!("Bowl: {:?}, slope: {:?}", bowl, bowl.slope());
         }
 
-        assert_eq!(solve(bowls), 30.)
+        assert_eq!(brute_solve(&mut bowls), 30.)
     }
 
     #[test]
-    fn test_solve_floor_case() {
+    fn test_gap_floor_case() {
         // P_11 case (although actually clamped to floor)
-        todo!()
+        let plate = Bowl::from([1, 100, 101]);
+        let bowl = Bowl::from([9, 1, 9]);
+
+        assert_eq!(find_gap(&plate, &bowl), BowlFloat::default());
     }
 
     #[test]
-    fn test_solve_ceiling_case() {
+    fn test_gap_ceiling_case() {
         // Ceiling case when the base of the upper bowl is sitting on the rim
-        todo!()
+        let plate = Bowl::from([1, 100, 101]);
+        let bowl = Bowl::from([9, 1, 9]);
+
+        assert_eq!(find_gap(&bowl, &plate), plate.height as BowlFloat);
     }
 
     #[test]
-    fn test_solve_contact_lower_rim() {
+    fn test_gap_contact_lower_rim() {
         // P_12 case
-        todo!()
+        let bottom = Bowl::from([1, 1, 70]);
+        let top = Bowl::from([1, 1, 9]);
+
+        assert_eq!(find_gap(&bottom, &top), BowlFloat::default());
+
+        assert_eq!(solve_stack_for_sort(&mut vec![bottom, top], CMP_STEEPER), bottom.height as BowlFloat);
+        assert!(solve_stack_for_sort(&mut vec![bottom, top], CMP_SHALLOWER) >  bottom.height as BowlFloat);
     }
 
     #[test]
-    fn test_solve_contact_upper_base() {
+    fn test_gap_contact_upper_base() {
         // P_21 case
         todo!()
     }
 
     #[test]
-    fn test_solve_contact_upper_rim() {
+    fn test_gap_contact_upper_rim() {
         // P_22 case
         todo!()
+    }
+    
+    fn test_solve_adversarial_tiny_troll_bowl() {
+        // if a bowl is so tiny that it could sit inside the final bowl, it doesn't matter what it's slope is
         
+        // tiny bowl with slope 4, plate with slope 2, bowl with slope 1 (don't put it on bottom)
+        
+        // tiny bowl with slope 1, plate with slope 2, bowl with slope 4
+        
+        // tiny bowl with slope 2, plate with slope 1, bowl with slope 4
+        
+        // 
+        
+        
+        let mut case_bowls: Vec<Bowl> = vec![];
+    }
+
+    #[test]
+    fn test_solve_nested_kattis_example() {
+        let mut case_bowls: Vec<Bowl> = vec![
+            Bowl::from([60, 20, 30]),
+            Bowl::from([40, 10, 50]),
+            Bowl::from([50, 30, 80]),
+            Bowl::from([35, 25, 70]),
+            Bowl::from([40, 10, 90]),
+        ];
+
+        let answers = brute_solve(&mut case_bowls).trunc() as u32;
+        assert_eq!(answers, 81) // Wrong answer says it's 87.5 (outputting 87)  (brute_solve says 81)
+    }
+
+    // fn test_solve_same_heights() {
+    //     let mut bowls = vec![
+    //         Bowl::from([10, 2, 20]),
+    //         Bowl::from([10, 10, 15]),
+    //         Bowl::from([10, 12, 27]),
+    //         Bowl::from([10, 14, 29]),
+    //         Bowl::from([10, 30, 31]),
+    //         Bowl::from([10, 7, 51]),
+    //         Bowl::from([10, 9, 53]),
+    //     ];
+    // }
+    
+    #[test]
+    fn test_map_permutations_len() {
+        let mut bowls = vec![
+            Bowl::from([10, 2, 20]),
+            Bowl::from([10, 10, 15]),
+            Bowl::from([10, 12, 27]),
+            Bowl::from([10, 14, 29]),
+            Bowl::from([10, 30, 31]),
+            Bowl::from([10, 7, 51]),
+            Bowl::from([10, 9, 53]),
+            Bowl::from([10, 11, 55]),
+            Bowl::from([10, 13, 57]),
+        ];
+        
+        assert_eq!(bowls.map_permutations(|bowlstack| {BowlFloat::default()}).len(), 362880)
+    }
+    
+    #[test]
+    fn test_map_permutations_contents() {
+        let mut bowls: Vec<BowlInt> = vec![1,2,3];
+        let out = bowls.map_permutations(|x| {x.to_vec()});
+        assert_eq!(out, vec![vec![1,2,3], vec![1,3,2], vec![2,1,3], vec![2,3,1], vec![3,1,2], vec![3,2,1]]);
     }
 }

292abe73fb5e6defa8f03b755b583091d7ca9fee
Joshua Freudenhammer <jfreuden@alumni.cmu.edu>
2025-08-03 05:54:10 +0200

    Implement bowlstack. Correct for the two test cases I currently have implemented, failing the submission

--- progress/bowlstack/src/main.rs
+++ progress/bowlstack/src/main.rs
@@ -40,13 +40,23 @@ type BowlFloat = f32;
 
 #[derive(Debug, Copy, Clone)]
 struct Bowl {
-    height: BowlInt,
-    bottom_radius: BowlInt,
-    top_radius: BowlInt,
+    height: BowlFloat,
+    bottom_radius: BowlFloat,
+    top_radius: BowlFloat,
 }
 
 impl From<[BowlInt; 3]> for Bowl {
     fn from([height, bottom_radius, top_radius]: [BowlInt; 3]) -> Self {
+        Bowl {
+            height: height as BowlFloat,
+            bottom_radius: bottom_radius as BowlFloat,
+            top_radius: top_radius as BowlFloat,
+        }
+    }
+}
+
+impl From<[BowlFloat; 3]> for Bowl {
+    fn from([height, bottom_radius, top_radius]: [BowlFloat; 3]) -> Self {
         Bowl {
             height,
             bottom_radius,
@@ -60,7 +70,6 @@ impl Bowl {
         self.height as BowlFloat / (self.top_radius - self.bottom_radius) as BowlFloat
     }
 }
-
 
 trait Solvable<T> {
     fn solve(bowls: T) -> BowlFloat;
@@ -80,14 +89,12 @@ impl Solvable<Vec<Bowl>> for Vec<Bowl> {
 #[allow(dead_code)]
 const CMP_STEEPER: fn(&Bowl, &Bowl) -> core::cmp::Ordering = |a, b| {
     BowlFloat::total_cmp(&a.slope(), &b.slope()).then_with(|| {
-        BowlInt::cmp(&a.bottom_radius, &b.bottom_radius)
-            .then_with(|| BowlInt::cmp(&a.height, &b.height))
+        BowlFloat::total_cmp(&a.bottom_radius, &b.bottom_radius)
+            .then_with(|| BowlFloat::total_cmp(&a.height, &b.height))
     })
 };
 #[allow(dead_code)]
-const CMP_SHALLOWER: fn(&Bowl, &Bowl) -> core::cmp::Ordering = |a, b| {
-    CMP_STEEPER(b, a)
-};
+const CMP_SHALLOWER: fn(&Bowl, &Bowl) -> core::cmp::Ordering = |a, b| CMP_STEEPER(b, a);
 
 impl Solvable<&mut Vec<Bowl>> for &mut Vec<Bowl> {
     fn solve(bowls: &mut Vec<Bowl>) -> BowlFloat {
@@ -116,12 +123,11 @@ impl Solvable<&mut Vec<Bowl>> for &mut Vec<Bowl> {
             let p21x = r2 - r1;
             let p22x = R2 - r1;
 
-            // is it an optimization that p22y > p21y and that p12y > p11y?
             let p11y = BowlFloat::default();
             let p12y = y(m1, p12x, BowlFloat::default());
             let b_floor = -y(m2, p21x, BowlFloat::default());
-            
-            let b2 = if r2 <= r1 {
+
+            let b2 = if r2 <= r1 && y(m2, p22x, b_floor) >= y(m1, p22x, BowlFloat::default()) {
                 b_floor // Floor case -------- had let p21y = BowlFloat::default(); here, unsure if needed
             } else if r2 >= R1 {
                 b_floor + h2 // Ceiling case (or rim case, if you prefer)
@@ -131,14 +137,18 @@ impl Solvable<&mut Vec<Bowl>> for &mut Vec<Bowl> {
                 } else {
                     b(m1, m2, p21x) // Steeper only allows P_21 case
                 }
-            } else if m2 < m1 { // Shallow could be P_12 case or P_22 case
+            } else if m2 < m1 {
+                // Shallow could be P_12 case or P_22 case
                 if R2 > R1 {
                     b(m1, m2, p12x) // P_12 case
                 } else {
                     b(m1, m2, p22x) // P_22 case
                 }
             } else {
-                panic!("Holy shit! We are all gonna die, this case shouldn't happen, holy fucking hell, someone take a look at these demon bowls and tell me what's wrong! {:?}, {:?}", bottom, top)
+                panic!(
+                    "Holy shit! We are all gonna die, this case shouldn't happen, holy fucking hell, someone take a look at these demon bowls and tell me what's wrong! {:?}, {:?}",
+                    bottom, top
+                )
             };
 
             let p21y = y(m2, p21x, b2);
@@ -147,17 +157,36 @@ impl Solvable<&mut Vec<Bowl>> for &mut Vec<Bowl> {
             // Do I even need to check that P22 is higher than P12?
             // In theory, the gap is simply p21y, and the contact points were preselected.
             p21y
-    }
+        }
 
-        fn solve_stack_for_sort(bowls: &mut Vec<Bowl>, sort: fn(&Bowl, &Bowl) -> core::cmp::Ordering) -> f32 {
+        fn solve_stack_for_sort(
+            bowls: &mut Vec<Bowl>,
+            sort: fn(&Bowl, &Bowl) -> core::cmp::Ordering,
+        ) -> f32 {
             bowls.sort_by(sort);
             let mut stackheight = bowls.first().unwrap().height as BowlFloat;
             let mut highestbottom = BowlFloat::default();
+            let mut highestbowl = bowls.first().unwrap().clone();
             for window in bowls.windows(2) {
                 let [bottom, top]: [Bowl; 2] = window.try_into().unwrap();
-                let gap = find_gap(&bottom, &top);
-                highestbottom += gap;
-                stackheight = BowlFloat::max(highestbottom + top.height as BowlFloat, stackheight);
+                let gap_bottom = find_gap(&bottom, &top);
+
+                let synth_bowl = Bowl::from([
+                    stackheight - highestbottom,
+                    bottom.bottom_radius,
+                    highestbowl.top_radius,
+                ]);
+
+                let gap_highest = find_gap(&synth_bowl, &top);
+                highestbottom += BowlFloat::max(gap_bottom, gap_highest);
+
+                let candidate = highestbottom + top.height as BowlFloat;
+                if candidate > stackheight {
+                    stackheight = candidate;
+                    highestbowl = top.clone();
+                }
+
+                stackheight = BowlFloat::max(candidate, stackheight);
             }
             stackheight
         }
@@ -176,13 +205,13 @@ fn main() {
         let mut bowls: Vec<Bowl> = Vec::with_capacity(num_bowls as usize);
 
         for _ in 0..num_bowls {
-            bowls.push(Bowl::from(read_array()))
+            bowls.push(Bowl::from(read_array::<BowlInt, 3, _>()))
         }
         case_bowls.push(bowls);
     }
 
     for mut bowls in case_bowls {
-        let stackheight = solve(&mut bowls);
+        let stackheight = solve(&mut bowls).trunc() as u32;
         println!("{:?}", stackheight);
     }
 }
@@ -194,15 +223,12 @@ mod tests {
     #[test]
     fn test_solve_kattis_example() {
         let mut case_bowls: Vec<Vec<Bowl>> = vec![
-            vec![
-                Bowl::from([60, 20, 30]),
-                Bowl::from([40, 10, 50]),
-            ],
+            vec![Bowl::from([60, 20, 30]), Bowl::from([40, 10, 50])],
             vec![
                 Bowl::from([50, 30, 80]),
                 Bowl::from([35, 25, 70]),
                 Bowl::from([40, 10, 90]),
-            ]
+            ],
         ];
 
         let answers = case_bowls.iter_mut().map(solve).collect::<Vec<f32>>();
@@ -222,5 +248,36 @@ mod tests {
         }
 
         assert_eq!(solve(bowls), 30.)
+    }
+
+    #[test]
+    fn test_solve_floor_case() {
+        // P_11 case (although actually clamped to floor)
+        todo!()
+    }
+
+    #[test]
+    fn test_solve_ceiling_case() {
+        // Ceiling case when the base of the upper bowl is sitting on the rim
+        todo!()
+    }
+
+    #[test]
+    fn test_solve_contact_lower_rim() {
+        // P_12 case
+        todo!()
+    }
+
+    #[test]
+    fn test_solve_contact_upper_base() {
+        // P_21 case
+        todo!()
+    }
+
+    #[test]
+    fn test_solve_contact_upper_rim() {
+        // P_22 case
+        todo!()
+        
     }
 }

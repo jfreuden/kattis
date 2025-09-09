1ff19ec44e06906d1ce7022717f73b0e0e83bff6
Joshua Freudenhammer <jfreuden@alumni.cmu.edu>
2025-09-09 16:40:20 +0200

    Proceeding along the closed form for `princesse`

---
+++ progress/princesse_LUT_calc/src/main.rs
@@ -0,0 +1,23 @@
+fn calculate_k(penalty_value: u32, starting_k: f64) -> f64 {
+    let left_side = (penalty_value + 1) as f64;
+    let mut k = starting_k;
+
+    while left_side > k.ln() / (1.0f64 - k).ln() {
+        k -= 0.00000005f64;
+    }
+    k
+}
+
+fn main() {
+    println!("const SPLIT_LUT: [f32; 1001] = [");
+
+    // S + 1 = ln k / ln (1-k)
+    // For each S from 0 to 1000, calculate K for a LUT.
+    let mut last_value = 0.5f64;
+    for penalty in 0..=1000 {
+        last_value = calculate_k(penalty, last_value);
+
+        println!("{}f32, ", last_value as f32);
+    }
+    println!("];");
+}
--- progress/prinsesse/src/prinsesse_tests.rs
+++ progress/prinsesse/src/prinsesse_tests.rs
@@ -1,2 +1,168 @@
 use super::*;
 
+
+fn run_scenario(mattresses: u32, nights: u32, penalty: u32, pea_location: u32) -> u32 {
+    println!("{} {} {} ({})", mattresses, nights, penalty, pea_location);
+
+    let mut scenario = Scenario::new(
+        mattresses,
+        nights,
+        penalty,
+        std::io::stdin().lock(),
+        std::io::stdout(),
+    );
+
+    while scenario.configuration.mattresses > 1 && scenario.configuration.nights > 0 {
+        scenario.autonomous_step(pea_location);
+    }
+    println!("! {}", scenario.configuration.offset);
+
+    assert_eq!(pea_location, scenario.configuration.offset);
+
+    scenario.configuration.offset
+}
+
+#[test]
+fn test_group1() {
+    let [mattresses, nights, penalty]: [u32; 3] = [2, 2, 1];
+    run_scenario(mattresses, nights, penalty, 0);
+    run_scenario(mattresses, nights, penalty, 1);
+}
+
+#[test]
+fn test_group2() {
+    run_scenario(3,3,0, 0);
+    run_scenario(3,3,0, 1);
+    run_scenario(3,3,0, 2);
+
+    run_scenario(1000,1000,365, 0);
+    run_scenario(1000,1000,365, 1);
+    run_scenario(1000,1000,365, 565);
+    run_scenario(1000,1000,365, 998);
+    run_scenario(1000,1000,365, 999);
+
+    run_scenario(1000,1000,1000, 999);
+}
+
+#[test]
+fn test_group3() {
+    let mattresses = 6;
+    let nights = 4;
+    let penalty = 1;
+
+    for mattress in 0..mattresses {
+        run_scenario(mattresses, nights, penalty, mattress);
+    }
+}
+
+#[test]
+fn test_group4() {
+    let mattresses = 100;
+    let nights = 7;
+    let penalty = 0;
+
+    for mattress in 0..mattresses {
+        run_scenario(mattresses, nights, penalty, mattress);
+    }
+}
+
+#[test]
+fn test_group5() {
+    let mattresses = 100;
+    let nights = 30;
+    let penalty = 5;
+
+    for mattress in 0..mattresses {
+        run_scenario(mattresses, nights, penalty, mattress);
+    }
+}
+
+#[test]
+fn test_group6_0100() {
+    let [mattresses, nights] = [1000, 72];
+    let penalty = 31;
+    for mattress in 0..100 {
+        run_scenario(mattresses, nights, penalty, mattress);
+    }
+}
+
+#[test]
+fn test_group6_0200() {
+    let [mattresses, nights] = [1000, 72];
+    let penalty = 31;
+    for mattress in 100..200 {
+        run_scenario(mattresses, nights, penalty, mattress);
+    }
+}
+
+#[test]
+fn test_group6_0300() {
+    let [mattresses, nights] = [1000, 72];
+    let penalty = 31;
+    for mattress in 200..300 {
+        run_scenario(mattresses, nights, penalty, mattress);
+    }
+}
+
+#[test]
+fn test_group6_0400() {
+    let [mattresses, nights] = [1000, 72];
+    let penalty = 31;
+    for mattress in 300..400 {
+        run_scenario(mattresses, nights, penalty, mattress);
+    }
+}
+
+#[test]
+fn test_group6_0500() {
+    let [mattresses, nights] = [1000, 72];
+    let penalty = 31;
+    for mattress in 400..500 {
+        run_scenario(mattresses, nights, penalty, mattress);
+    }
+}
+
+#[test]
+fn test_group6_0600() {
+    let [mattresses, nights] = [1000, 72];
+    let penalty = 31;
+    for mattress in 500..600 {
+        run_scenario(mattresses, nights, penalty, mattress);
+    }
+}
+
+#[test]
+fn test_group6_0700() {
+    let [mattresses, nights] = [1000, 72];
+    let penalty = 31;
+    for mattress in 600..700 {
+        run_scenario(mattresses, nights, penalty, mattress);
+    }
+}
+
+#[test]
+fn test_group6_0800() {
+    let [mattresses, nights] = [1000, 72];
+    let penalty = 31;
+    for mattress in 700..800 {
+        run_scenario(mattresses, nights, penalty, mattress);
+    }
+}
+
+#[test]
+fn test_group6_0900() {
+    let [mattresses, nights] = [1000, 72];
+    let penalty = 31;
+    for mattress in 800..900 {
+        run_scenario(mattresses, nights, penalty, mattress);
+    }
+}
+
+#[test]
+fn test_group6_1000() {
+    let [mattresses, nights] = [1000, 72];
+    let penalty = 31;
+    for mattress in 900..1000 {
+        run_scenario(mattresses, nights, penalty, mattress);
+    }
+}

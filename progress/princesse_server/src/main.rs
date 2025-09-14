2d380ab5167a9fc78e4ce10eebcae9222665c37a
Joshua Freudenhammer <jfreuden@alumni.cmu.edu>
2025-09-14 02:40:10 +0200

    added more functionality to prinsesse server

--- progress/princesse_server/src/main.rs
+++ progress/princesse_server/src/main.rs
@@ -1,12 +1,3 @@
-fn read_one<T: std::str::FromStr>() -> T
-where
-    T::Err: std::fmt::Debug,
-{
-    let mut line = String::new();
-    std::io::stdin().read_line(&mut line).unwrap();
-    line.trim().parse::<T>().unwrap()
-}
-
 fn read_vec<T: std::str::FromStr>() -> Vec<T>
 where
     T::Err: std::fmt::Debug,
@@ -38,9 +29,36 @@ fn main() {
             std::process::exit(1);
         }
 
-        if line[0] != "?" {
+        if line[0] == "#" {
+            println!("{} nights remaining", nights);
+            continue;
+        } else if line[0] != "?" {
             eprintln!("Invalid input");
             std::process::exit(1);
+        } else if line[1].contains("...") {
+            let [range_min, range_max] = line[1].split("...").map(|x|x.parse::<u32>().unwrap()).collect::<Vec<u32>>().try_into().unwrap();
+            if range_min > range_max || range_max >= starting_mattresses {
+                eprintln!("range invalid");
+                std::process::exit(1);
+            }
+            println!("Querying singles from {} through {}", range_min, range_max);
+            for i in range_min..=range_max {
+                println!("? {}", i);
+
+                if i == pea_location {
+                    nights -= 1 + penalty as i32;
+                    println!("1");
+                    break;
+                } else {
+                    nights -= 1;
+                    println!("0");
+                }
+
+                if nights < 0 {
+                    println!("Ran out of nights");
+                    std::process::exit(1);
+                }
+            }
         } else if line[1].contains("..") {
             // Using custom range notation
             let [range_min, range_max] = line[1].split("..").map(|x|x.parse::<u32>().unwrap()).collect::<Vec<u32>>().try_into().unwrap();
